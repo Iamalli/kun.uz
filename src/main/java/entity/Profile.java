f6b2fdfb577e292ba8b3945aee0293ce6f5e33d9
alibek.shomurotov <alikosomurodovic7@gamil.com>
2025-05-22 18:03:19 +0500

    Go project

---
+++ src/main/java/entity/Profile.java
@@ -0,0 +1,27 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+}
